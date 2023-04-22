13689dbb37497e0b8eab4796ef835bcdbb915659
TravisUserName <103704083+TravisUserName@users.noreply.github.com>
2023-04-22 12:46:56 -0400

    Add files via upload

--- CreateTable.sh
+++ CreateTable.sh
@@ -2,7 +2,7 @@ gcloud sql connect finalprojectsql
 
 use finalprojectdb
 
-CREATE TABLE `environment2` (
+CREATE TABLE `environment` (
   `id` int NOT NULL AUTO_INCREMENT,
   `temp` float NOT NULL,
   `humidity` float NOT NULL,
